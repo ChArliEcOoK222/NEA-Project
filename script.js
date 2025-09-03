9c281921a923ccfb559d5bc2f5b63327853a3baf
Charlie C <charlie2007.cook@gmail.com>
2025-09-03 14:41:03 +0100

    Added the shot information dropdown, fixed issue of player identification

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-let events = []; // global array to store all the recorded event coordinates
+let shots = []; // global array to store all the recorded shot coordinates
 
 
 // function to get called on the mouse event of clicking
-function recordEvent(event) {
+function recordShot(event) {
     // normalising the x coordinate of the event
     const x = (event.offsetX / event.target.offsetWidth) * 68;
     // flipping and scaling y 
     const y_flipped = 105 - (event.offsetY / event.target.offsetHeight) * 105;
-    // saving the coordinates the the events array 
-    events.push([x, y_flipped])
+    // capture the current timestamp
+    const timestamp = new Date().toISOString();
+    // saving the coordinates and timestamp to the shots array 
+    shots.push([x, y_flipped, timestamp])
     // creating a visual marker on the football pitch
     let marker = document.createElement('div')
-    // adding a class name for styling in order to ensure that events appear on the pitch
-    marker.className = 'event-marker'
+    // adding a class name for styling in order to ensure that shot markers appear on the pitch
+    marker.className = 'shot-marker'
     // positioning the marker horizontally 
     marker.style.left = (x / 68 * 100) + '%';
     // positioning the marker vertically 
@@ -20,7 +22,11 @@ function recordEvent(event) {
     marker.style.top = y_original_percentage + '%';
     // add a marker to the element
     event.target.appendChild(marker);
-    
+
+    const informationPopup = document.getElementById('informationPopup');
+    informationPopup.style.display = 'block';
 }
+
+
 
 
