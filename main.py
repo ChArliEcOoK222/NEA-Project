100dd307ac74f68e30567f7280435a5dbca0ba4b
Charlie C <charlie2007.cook@gmail.com>
2025-10-07 19:51:33 +0100

    Trained the model again on a new dataset, ready to be tested on the User Interface

--- main.py
+++ main.py
@@ -9,77 +9,39 @@ import joblib
 import os
 import json
 
-# Path the json events folder
-path = "/Users/charlie/Documents/NEA Project II/open-data/data/events"
-# List to store the events data
-data = []
+# Loading the datasets
+training_dataset = "/Users/charlie/Documents/NEA Project II/Football-Shots-Dataset-for-xG-models/data/Shots.csv"
+# Saving the dataset as a DataFrame
+training_shots = pd.read_csv(training_dataset)
 
-# Iterating over all files in the events path
-for file in os.listdir(path):
+# Variables to be used when calculating the distance from the goal and angle of the shot
+goal_x = 1.0
+goal_y = 0.5
+goal_width = 0.073
+goal_top_y = goal_y + goal_width / 2
+goal_bottom_y = goal_y - goal_width / 2
+goal_span = goal_top_y - goal_bottom_y
 
-    # Code can only run if the file is JSON
-    if not file.endswith(".json"):
-        continue
+# Calculating angle and distance
+training_shots['Angle'] = np.arctan2(goal_span, goal_x - training_shots[' X'])
+training_shots['Distance'] = np.sqrt((goal_x - training_shots[' X'])**2 + (goal_y - training_shots[' Y'])**2)
 
-    # Creating the specific path name of the file
-    file_path = os.path.join(path, file)
-    # Reading the content of the json file
-    with open(file_path, "r") as content:
-        events = json.load(content)
+# Encoding the shot types 
+type_encoder = OneHotEncoder(sparse_output=False)
+encoded_shots = type_encoder.fit_transform(training_shots[[' shotType']])
+encoded_shot_types = pd.DataFrame(encoded_shots, columns=type_encoder.get_feature_names_out([' shotType']))
 
-    # Iterating over the content to locate the shots
-    for event in events:
-        try:
-            # Locating the shots from within the file
-            if event ["type"]["name"] == "Shot":
-                # Attempts to access location 
-                loc = event.get("location", [None, None])
-                # Saving the relevant data to a new variable
-                shot = {
-                    "x": loc[0],
-                    "y": loc[1],
-                    "body_part": event["shot"]["body_part"]["name"],
-                    "outcome": event["shot"]["outcome"]["name"],
-                    "statsbomb_xg": event["shot"]["statsbomb_xg"]["name"]
-                }
-                # Appending the data to the list
-                data.append(shot)
-        # Ensuring the code does not crash
-        except (TypeError, IndexError, KeyError):
-            pass
-
-# Loading the data into a pandas Dataframe
-shots = pd.DataFrame(data)
-
-# Calculating the distance from the goal and angle of the shot
-shots['Angle'] = np.arctan(np.absolute(shots['x'] - 40) / np.absolute(shots['y'] - 120))
-shots['Distance'] = np.sqrt((shots['x'] - 120)**2 + (shots['y'] - 40)**2)
-
-# Encoding shot type as binary
-encoder = OneHotEncoder(sparse=False)
-shot_type_encoded = encoder.fit_transform(shots[['body_part']])
-shot_types = pd.DataFrame(shot_type_encoded, columns=encoder.get_feature_names_out(['body_part']))
-
-# Creating a binary column
-shots["Binary"] = 0
-# Assigning a binary value to each shot
-for index, row in shots.iterrows():
-    if row['outcome'] == 'Goal':
-        shots.at[index, 'Binary'] = 1
+# Creating a binary column for the shot outcome
+training_shots['Binary'] = training_shots.apply(
+    lambda row: 1 if (row[' h_a'] == 1 and row[' h_goals'] == 1) or 
+                    (row[' h_a'] == 0 and row[' a_goals'] == 1) else 0,
+    axis=1
+)
 
 # Training and saving the model
-X = pd.concat([shots[['Distance', 'Angle']], shot_types], axis=1)
-y = shots["Binary"]
+X = pd.concat([training_shots[['Distance', 'Angle']], encoded_shot_types], axis=1)
+y = training_shots['Binary']
 xGmodel = LogisticRegression()
 xGmodel.fit(X, y)
 # Saving the model for future use
-joblib.dump(xGmodel)
-
-
-
-# Calculating expected goals
-xG = xGmodel.predict_proba(shots[['Distance', 'Angle']])[:,1]
-shots['xG'] = xG
-shots.to_csv('expected goals.csv')
-
-
+joblib.dump(xGmodel, 'xG_model.pkl')
