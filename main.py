c748ab3828906b9da6d9486ade2efe0861bd8cb8
Charlie C <charlie2007.cook@gmail.com>
2025-10-12 17:16:27 +0100

    Second version of the model works but is inaccurate

--- main.py
+++ main.py
@@ -21,6 +21,18 @@ type_encoder = OneHotEncoder(sparse_output=False)
 encoded_shots = type_encoder.fit_transform(shots[['type']])
 encoded_shot_types = pd.DataFrame(encoded_shots, columns=type_encoder.get_feature_names_out(['type']))
 
+# Renaming encoded columns
+new_names = {'type_Left_Footed': ' shotType_1.0', 'type_Right_Footed': ' shotType_2.0', 'type_Headed': ' shotType_3.0', 'type_Other': ' shotType_4.0'}
+
+encoded_shot_types.rename(columns=new_names, inplace=True)
+
+# Ensure all expected columns exist, even if missing in current data
+for col in [' shotType_1.0', ' shotType_2.0', ' shotType_3.0', ' shotType_4.0']:
+    if col not in encoded_shot_types.columns:
+        encoded_shot_types[col] = 0  # fill with zeros
+
+encoded_shot_types = encoded_shot_types[[' shotType_1.0', ' shotType_2.0', ' shotType_3.0', ' shotType_4.0']]
+
 # Loading the trained expected goals model
 xGmodel = joblib.load('xG_model.pkl')
 
